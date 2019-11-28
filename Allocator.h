3395257cfa09b93acab8476a15f305149d9d1cc6
weee <wtdreamer@163.com>
2019-11-28 19:35:59 +0800

    init

---
+++ Allocator.h
@@ -0,0 +1,11 @@
+/*
+* 负责内存空间的分配和释放
+*/
+
+#ifndef ALLOCATOR_H_
+#define ALLOCATOR_H_
+
+#include <new>
+#include <stdlib.h>
+
+#endif // ALLOCATOR_H_
---
+++ Construct.h
@@ -0,0 +1,10 @@
+/*
+* 负责对象内容的构造和析构
+*/
+
+#ifndef CONSTRUCT_H_
+#define CONSTRUCT_H_
+
+
+
+#endif // CONSTRUCT_H_
